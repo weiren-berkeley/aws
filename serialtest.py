5fe57d2bba984c90f54725f68b8ee6593baa707f
Wei Ren <weiren@berkeley.edu>
2018-03-04 21:13:47 -0800

    7data

--- serialtest.py
+++ serialtest.py
@@ -12,8 +12,22 @@ try:
     print 'Starting sending data from uart.'
     while True:
         time.sleep(1)
-        angle1 = random.random()
-        ser.write('Angle1 = '+ str(angle1) + ';\r\n')
+        angle1 = random.random() + 1
+        angle2 = random.random() + 2
+        angle3 = random.random() + 3
+        angle4 = random.random() + 4
+        angle5 = random.random() + 5
+        angle6 = random.random() + 6
+        angle7 = random.random() + 7
+        ser.write('B')
+        ser.write(str(angle1) + '\r\n')
+        ser.write(str(angle2) + '\r\n')
+        ser.write(str(angle3) + '\r\n')
+        ser.write(str(angle4) + '\r\n')
+        ser.write(str(angle5) + '\r\n')
+        ser.write(str(angle6) + '\r\n')
+        ser.write(str(angle7) + '\r\n')
+        ser.write('E')
         if ser.inWaiting() > 0:
             data = ser.readline()
             print data
